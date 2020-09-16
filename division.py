0b77e3a9e7f16ae8b7d6574f8c73de33a28248d8
Jessica Kimbril <57114403+jessicakimbril@users.noreply.github.com>
2020-09-15 21:36:06 -0500

    progress

---
+++ division.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(a // b) #* divide a by b and get the result of integer division
+print(a / b) #* divide a by b and get the result of float division
